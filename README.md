160be699b608aa751611470aae9b0a6533695528
sting <stinggyray@gmail.com>
2023-03-08 17:45:37 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Algorithms
+
+On my quest to become a half-decent coder, I'm going to try to implement
+every algorithm I understand in this repository.
+
+Well, not EVERY algorithm. Just the stuff for competitive programming. No MergeSort or BogoSort or bubble sort or whatever.
---
+++ src/graphs/dijkstra.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int (i) = a; (i) < (b); ++(i))
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+/*
+ * Implementation of (some algorithm).
+ */
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+
+
+	cout << "" << "\n";
+}
